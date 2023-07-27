708b1565a6cf000483cd1ace5b919bcd301941c8
gzwongkk <gzwongkk@gmail.com>
2023-07-27 09:59:23 +0800

    Update bibliography

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
+    $('a.tldr').click(function() {
+        $(this).parent().parent().find(".tldr.hidden").toggleClass('open');
+        $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
+        $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
+    });
     $('a.abstract').click(function() {
+        $(this).parent().parent().find(".tldr.hidden.open").toggleClass('open');
         $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
         $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
     });
     $('a.bibtex').click(function() {
-        $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
+        $(this).parent().parent().find(".tldr.hidden.open").toggleClass('open');
         $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
+        $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     });
     $('a').removeClass('waves-effect waves-light');
 });
